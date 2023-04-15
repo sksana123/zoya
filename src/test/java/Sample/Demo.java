c0220a33b5c65374ee5a47a432487e29e715da14
PC <shaiksana9696@gmail.com>
2023-04-15 13:18:26 +0530

    commited

---
+++ src/test/java/Sample/Demo.java
@@ -0,0 +1,18 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+@Test
+	
+	public void demo()
+	{
+		System.out.println("this is demo pgm");
+		
+		
+		
+		System.out.println("--------");
+	}
+
+
+}
